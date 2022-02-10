dcaa7e29c58144334d8bec94d2c4fd336dba8298
Amal Mathur <68689223+Amal-Mat@users.noreply.github.com>
2022-02-09 21:29:51 -0500

    Added YML file and removed comments which were not needed

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ const app = express();
 app.use("/",router);
 
 const port = process.env.PORT || 3000;
-/* app.listen(port, async () => {
-    console.log(`Server is running on port ${port}`);
-}); */
-
-/* if(!module.parent){
-    app.listen(3000);
-} */
 
 module.exports = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
