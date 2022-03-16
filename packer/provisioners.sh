20a52f317da0ee38c12bb5c1a45f8be2fbf901e5
Amal Mathur <68689223+Amal-Mat@users.noreply.github.com>
2022-03-16 19:17:33 -0400

    Added features of uploading an image and deleting it as part of Assignment#5

--- packer/provisioners.sh
+++ packer/provisioners.sh
@@ -3,19 +3,19 @@
 sleep 30
 
 sudo yum update -y
-
+sleep 10
 sudo yum install git make gcc -y
+sleep 10
 sudo amazon-linux-extras install epel
+sleep 10
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash ~/.nvm/nvm.sh
 sudo yum install -y gcc-c++ make
+sleep 10
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum install -y nodejs
+sleep 10
 sudo npm install -g pm2
+sleep 10
 
-# sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
-# sudo yum -y install mysql-community-server
-# sudo systemctl enable --now mysqld
-# systemctl status mysqld
-# mysql -u root --password=Boston@2021
 mkdir /home/ec2-user/node-app
 chown ec2-user:ec2-user /home/ec2-user/node-app
--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const baseAuthentication = require('../util/auth.js');
 const userController = require('../Controller/usersController.js');
+const imageController = require('../Controller/imageController.js');
+const multer = require('multer');
 
 // GET Method
 
@@ -20,5 +22,21 @@ router.get("/v1/user/self", baseAuthentication() , userController.getUser);
 // PUT Method
 
 router.put("/v1/user/self", baseAuthentication() , userController.updateUser);
+
+// POST Method (Image)
+
+const upload = multer({
+    dest: 'uploads/'
+})
+
+router.post("/v1/user/self/pic", baseAuthentication(), upload.single('file'), imageController.updateUserPic);
+
+// GET Method (Image)
+
+router.get("/v1/user/self/pic", baseAuthentication(), imageController.getUserPic);
+
+// DELETE Method (Image)
+
+router.delete("/v1/user/self/pic", baseAuthentication(), imageController.deleteUserPic);
 
 module.exports = router; 
